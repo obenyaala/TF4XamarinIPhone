a4c827fdfe722a878f507651535ad8a4c583e0b6
Oussama Benyaala <WN00151959@wgs.wuerth.com>
2021-07-30 12:07:46 +0200

    Iphone T4 File error

---
+++ TF4FileIphoneTest/TF4FileIphoneTest/TF4FileIphoneTest/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using TF4FileIphoneTest.T4;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TF4FileIphoneTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent(); 
+            InitializeLocalizationResourceManager();
+            MainPage = new MainPage();
+        }
+        private void InitializeLocalizationResourceManager()
+        {
+            LocalizationResourceManager.Current.PropertyChanged -= LanguageChanged;
+            LocalizationResourceManager.Current.PropertyChanged += LanguageChanged;
+            LocalizationResourceManager.Current.Init(AppResources.ResourceManager);
+        }
+
+        private void LanguageChanged(object sender, PropertyChangedEventArgs e)
+        {
+            AppResources.Culture = LocalizationResourceManager.Current.CurrentCulture;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TF4FileIphoneTest/TF4FileIphoneTest/TF4FileIphoneTest/T4/AppResources.cs
@@ -0,0 +1,58 @@
+﻿
+namespace TF4FileIphoneTest.T4
+{
+    using System;
+
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+
+    internal class AppResources
+    {
+
+        private static global::System.Resources.ResourceManager resourceMan;
+        
+        private static global::System.Globalization.CultureInfo resourceCulture;
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal AppResources()
+        {
+        }
+
+        /// <summary>
+        ///   Returns the cached ResourceManager instance used by this class.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TF4FileIphoneTest.T4.AppResources", typeof(AppResources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        /// <summary>
+        ///   Overrides the current thread's CurrentUICulture property for all
+        ///   resource lookups using this strongly typed resource class.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a string similar to 'From T4File'
+        /// </summary>
+        internal static string Key
+        {
+            get { return nameof(Key); }
+        }
+    }
+}
